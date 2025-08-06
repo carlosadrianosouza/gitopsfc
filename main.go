e217c8914bbd890b6ef2f3c9efd4fd7459922893
Carlos Adriano <souza.adriano.carlos@gmail.com>
2025-08-06 07:35:02 -0300

    commit 0730

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Hello Argo - 06082025 - 0726</h1>"))
+		w.Write([]byte("<h1>Hello Argo - 06082025 - 0730</h1>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
